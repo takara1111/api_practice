ad64c64d40becafc4f7f2841da254a11501a05a2
takara1356 <goblin7283@gmail.com>
2019-08-15 10:36:19 +0900

    Add i18n

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module ApiPractice
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # エラメの日本語化
+    config.i18n.default_local = :ja
   end
 end
