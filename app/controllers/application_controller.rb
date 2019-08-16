98cbd36da92a29d3d01d21df8889043f309ee298
takara1356 <goblin7283@gmail.com>
2019-08-16 17:07:16 +0900

    fix authenticate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::API
     def authenticate
       if request.headers["Authenticate"].present?
         token = request.headers['Authorization'].split(' ').last
-
         @user = JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'RS256' })
+      else
+        render json: {status: 401, message: "ログインしろ"}
       end
     end
 end
