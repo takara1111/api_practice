812fd4b6794c103a28e25e69d48f2d5a22f2bee6
takara1356 <goblin7283@gmail.com>
2019-08-16 16:26:01 +0900

    add some actions to todos_controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,26 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: todo
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: {status: 200, message: 'タスクを作成しました'}
+    else
+      render json: {status: 400, message: @todo.errors.full_messages}
+    end
+  end
+
+    private
+
+    def todo_params
+      params.fetch(:todo, {}).permit(:title, :description, :status, :user_id)
+    end
 end
