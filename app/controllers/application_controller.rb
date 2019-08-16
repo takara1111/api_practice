1f12439012fce8641edcbe191e7bdf5f0b6c6ca8
takara1356 <goblin7283@gmail.com>
2019-08-16 17:05:20 +0900

    add authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  
+
+    private
+
+    def authenticate
+      if request.headers["Authenticate"].present?
+        token = request.headers['Authorization'].split(' ').last
+
+        @user = JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'RS256' })
+      end
+    end
 end
